41f8a01c220f824d6c50bb606c3acb63b5731dbe
Gustavo <johnnygar98@hotmail.com>
2019-10-21 22:28:51 -0500

    Nuevo inicio de limit en slide

--- src/main/java/com/shopshopista/productoss/controladores/producto/ProductosControlador.java
+++ src/main/java/com/shopshopista/productoss/controladores/producto/ProductosControlador.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/v1/producto")
-@CrossOrigin
+@CrossOrigin("*")
 public class ProductosControlador {
 
     @Autowired
